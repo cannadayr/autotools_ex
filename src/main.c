5aadd8ae218d22fefa1bd5bb5a1a000c479ebcce
Rowan Cannaday <cannadayr@gmail.com>
2016-07-08 02:02:03 -0500

    Sample ext lib using curl.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<curl/curl.h>
 #include"struct.h"
 
 int main() {
@@ -7,5 +8,30 @@ int main() {
 	x.is_retired = 1;
 
 	printf("The object x is_retired=%d\n",x.is_retired);
+
+	CURL *curl;
+	CURLcode res;
+
+	curl = curl_easy_init();
+	if(curl) {
+
+		curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+
+		/* example.com is redirected, so we tell libcurl to follow redirection */
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+		/* Perform the request, res will get the return code */
+		res = curl_easy_perform(curl);
+
+		/* Check for errors */
+		if(res != CURLE_OK) {
+		  fprintf(stderr, "curl_easy_perform() failed: %s\n",
+				  curl_easy_strerror(res));
+		}
+
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+	}
+
 	return 0;
 }
